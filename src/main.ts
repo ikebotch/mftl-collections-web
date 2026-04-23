171ae52447fe2adbc3e6190a0802f69b614d215b
ikebotch <ikebotch@gmail.com>
2026-04-24 00:37:08 +0100

    feat: initial scaffold

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { router } from './app/router'
+import { createPinia } from 'pinia'
+import './styles/index.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(createPinia())
+
+app.mount('#app')
